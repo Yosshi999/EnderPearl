1b67bed76e5fc75e5b21d39db31d1395efa6fc99
Yosshi999 <Yosshi999@users.noreply.github.com>
2015-01-03 17:05:56 +0900

    bugfix,improve

--- src/EnderPerl/Main.php
+++ src/EnderPerl/Main.php
@@ -31,11 +31,11 @@ class Main extends PluginBase implements Listener{
 		$entity = $event->getEntity();
 		if($entity instanceof Snowball){
 			$shooter = $entity->shootingEntity;
+			$ballid = $entity->getId();
 			if($shooter instanceof Player){
 				$id = $shooter->getId();
-//				$this->getLogger()->info($id);
-				if( array_key_exists($id,$this->order) ){$this->order[$id]++;}
-					else{$this->order += array($id => 1);}
+				if( array_key_exists($id,$this->order) ){array_push($this->order[$id],$ballid);}
+					else{$this->order += array($id => array($ballid));}
 			}
 		}
 	}
@@ -45,25 +45,35 @@ class Main extends PluginBase implements Listener{
 		if($entity instanceof Player){
 			$id = $entity->getId();
 //			$this->getLogger()->info($entity->getName()."is dead");
-			if(array_key_exists($id,$this->order)){$this->order[$id]=0;}
+			if(array_key_exists($id,$this->order)){$this->order[$id]=array();}
 		}
 	}
 	
 	public function onEntityClose(EntityDespawnEvent $event){
 		if($event->getType() === 81){	//81=Snowball
 			$entity = $event->getEntity();
+			$ballid = $entity->getId();
 			$shooter = $entity->shootingEntity;
 			$posTo = $entity->getPosition();
 			
 			if($shooter instanceof Player && $posTo instanceof Position){
 				$id = $shooter->getId();
-				if(array_key_exists($id,$this->order) && $this->order[$id]>0){
-					$this->order[$id]--;
+				$key = array_search($ballid,$this->order[$id]);
+				if(array_key_exists($id,$this->order) && $key!==false ){
+					unset($this->order[$id][$key]);
 					$posFrom = $shooter->getPosition();
 //					$this->getLogger()->info($shooter->getName()." is at ".$posTo->__toString() );
 					$shooter->teleport($posTo);
 					$shooter->attack(5);
 				}
+			}
+		}
+		if( $event->isHuman() ){		// log out
+			$entity = $event->getEntity();
+			$id = $entity->getId();
+			if(array_key_exists($id,$this->order)){
+				$this->getLogger()->info($entity->getName());
+				unset($this->order[$id]);
 			}
 		}
 	}
